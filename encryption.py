c97d2f656cd042457f9e44dae96f2409ee6a2bfc
joshpseb <144298690+joshpseb@users.noreply.github.com>
2026-03-13 15:26:38 -0500

    Implemented vigenere encryption and decryption

--- encryption.py
+++ encryption.py
@@ -1,42 +1,73 @@
 import sys
 
-passkey = None
+def vigenereEncrypt(text, key):
+    res = []
 
-for line in sys.stdin:
+    for i, char in enumerate(text):
+        # Convert letters to numbers 0-25
+        plain = ord(char) - ord('A')
+        shift = ord(key[i % len(key)]) - ord('A')
 
-    # gets rid of whitespace
-    line = line.strip()
+        cipher = (plain + shift + 26) % 26
+        res.append(chr(cipher + ord('A')))
 
-    # only splits once based on first space
-    parts = line.split(maxsplit=1)
-    command = parts[0]
+    return "".join(res)
 
-    if len(parts) > 1:
-        argument = parts[1]
-    else:
-        argument = ""
+def vigenereDecrypt(text, key):
+    res = []
 
-    if command == "QUIT":
-        break
+    for i, char in enumerate(text):
+        # Convert letters to numbers 0-25
+        cipher = ord(char) - ord('A')
+        shift = ord(key[i % len(key)]) - ord('A')
+        
+        # Reverse the cipher math and convert back to a letter
+        plain = (cipher - shift + 26) % 26
+        res.append(chr(plain + ord('A')))
+        
+    return "".join(res)
 
-    elif command == "PASS":
-        passkey = argument
-        print("RESULT: New passkey set")
-    
-    elif command == "ENCRYPT":
-        if not passkey:
-            print("ERROR: Passkey not set")
+def main():
+    passkey = None
+
+    for line in sys.stdin:
+
+        # gets rid of whitespace
+        line = line.strip()
+
+        # only splits once based on first space
+        parts = line.split(maxsplit=1)
+        command = parts[0]
+
+        if len(parts) > 1:
+            argument = parts[1]
         else:
-            res = vigenereEncrypt(argument, passkey)
-            print("RESULT: ", res)
+            argument = ""
 
-    
-    elif command == "DECRYPT":
-        if not passkey:
-            print("ERROR: Passkey not set")
+        if command == "QUIT":
+            break
+
+        elif command == "PASS":
+            passkey = argument
+            print("RESULT: New passkey set")
+        
+        elif command == "ENCRYPT":
+            if not passkey:
+                print("ERROR: Passkey not set")
+            else:
+                res = vigenereEncrypt(argument, passkey)
+                print("RESULT: ", res)
+
+        
+        elif command == "DECRYPT":
+            if not passkey:
+                print("ERROR: Passkey not set")
+            else:
+                res = vigenereDecrypt(argument, passkey)
+                print("RESULT: ", res)
+        
         else:
-            res = vigenereDecrypt(argument, passkey)
-            print("RESULT: ", res)
-    
-    else:
-        print("ERROR: Invalid command")
+            print("ERROR: Invalid command")
+
+if __name__ == "__main__":
+    main()
